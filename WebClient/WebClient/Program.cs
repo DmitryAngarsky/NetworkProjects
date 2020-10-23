49d342ccd3348c6b891d55f8d2eb696f28fb1782
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-10-23 22:11:27 +0400

    WebClient

---
+++ WebClient/WebClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace WebClient
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://translate.google.com");
+            HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync();
+            WebHeaderCollection headers = response.Headers;
+            
+            for (int i = 0; i < headers.Count; i++)
+            {
+                Console.WriteLine("{0}: {1}", headers.GetKey(i), headers[i]);
+            }
+            
+            response.Close();
+        }
+    }
+}
