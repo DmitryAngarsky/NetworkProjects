c6151aa309c05f85f278224f6738efabbeafb326
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-11-19 22:25:57 +0400

    Update UDPWebSocket

--- UDPWebSocket/UDPWebSocket/Program.cs
+++ UDPWebSocket/UDPWebSocket/Program.cs
@@ -2,6 +2,7 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace UDPWebSocket
 {
@@ -21,40 +22,75 @@ namespace UDPWebSocket
             
             Console.WriteLine("Для отправки сообщений введите сообщение и нажмите Enter");
             Console.WriteLine();
-            
-            
+
+            try
+            {
+                _listeningSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                Task listeningTask = new Task(Listen);
+                listeningTask.Start();
+
+                while (true)
+                {
+                    string message = Console.ReadLine();
+
+                    byte[] data = Encoding.Unicode.GetBytes(message);
+                    EndPoint remotePoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), _remotePort);
+                    _listeningSocket.SendTo(data, remotePoint);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Close();
+            }
         }
 
         private static void Listen()
         {
             try
             {
-                EndPoint localEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), _localPort);
-                _listeningSocket.Bind(localEndPoint);
-                
-                StringBuilder builder = new StringBuilder();
-                byte[] buffer = new byte[256];
-                
-                EndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
+                EndPoint localIp = new IPEndPoint(IPAddress.Parse("127.0.0.1"), _localPort);
+                _listeningSocket.Bind(localIp);
 
-                do
+                while (true)
                 {
-                    int bytes = _listeningSocket.ReceiveFrom(buffer, ref remoteEndPoint);
-                    builder.Append(Encoding.Unicode.GetString(buffer, 0, bytes));
-                } while (_listeningSocket.Available > 0);
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;
+                    byte[] buffer = new byte[256];
+                
+                    EndPoint remoteIp = new IPEndPoint(IPAddress.Any, 0);
 
-                if (remoteEndPoint is IPEndPoint remoteFullIp)
-                    Console.WriteLine("{0}:{1} - {2}", 
-                        remoteFullIp.Address,
-                        remoteFullIp.Port, builder);
+                    do
+                    {
+                        bytes = _listeningSocket.ReceiveFrom(buffer, ref remoteIp);
+                        builder.Append(Encoding.Unicode.GetString(buffer, 0, bytes));
+                    } while (_listeningSocket.Available > 0);
+                    
+                    IPEndPoint remoteFullIp = remoteIp as IPEndPoint;
+                    
+                    Console.WriteLine("{0}:{1} - {2}", remoteFullIp.Address, remoteFullIp.Port, builder);
+                }
             }
-            catch
+            catch(Exception ex)
             {
-
+                Console.WriteLine(ex.Message);
             }
             finally
             {
-                
+                Close();
+            }
+        }
+
+        private static void Close()
+        {
+            if (_listeningSocket != null)
+            {
+                _listeningSocket.Shutdown(SocketShutdown.Both);
+                _listeningSocket.Close();
+                _listeningSocket = null;
             }
         }
     }
